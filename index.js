055bb739b4a22143c40606eb4bd788dd61efd006
EegArlert <bsyahputra@oakland.edu>
2024-09-08 00:02:34 -0400

    creating the eventListener for mouseenter and change the color of the squares to black once the mouse hover it

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const gridContainer = document.querySelector(".container-grid");
 
-for(let i = 1; i <= 32; i++){
-    const div = document.createElement("div");
-    div.className = "square";
-    gridContainer.appendChild(div)
+
+//creating 16x16 grid via div
+for (let i = 1; i <= 16; i++) {
+    for (let k = 1; k <= 16; k++) {
+        const div = document.createElement("div");
+        div.className = "square";
+        gridContainer.appendChild(div)
+    }
 }
+
+const squares = document.querySelectorAll(".square");
+
+// Add event listener to each square
+squares.forEach(square => {
+    square.addEventListener("mouseenter", () => {
+        square.style.backgroundColor = "black"; // Color stays black after hover
+    });
+});
