bb5e6002bced78f231ab63ab72f72f6879a9ce86
EegArlert <bsyahputra@oakland.edu>
2024-09-08 01:04:45 -0400

    adding function when user change size, current childnodes will be deleted and replaced by the amount that user wants

--- index.js
+++ index.js
@@ -10,11 +10,44 @@ for (let i = 1; i <= 16; i++) {
     }
 }
 
-const squares = document.querySelectorAll(".square");
 
 // Add event listener to each square
-squares.forEach(square => {
-    square.addEventListener("mouseenter", () => {
-        square.style.backgroundColor = "black"; // Color stays black after hover
-    });
+startSketching();
+
+const button = document.querySelector(".pixel-size");
+const screenWidth = document.querySelector(".container-grid").width;
+console.log(screenWidth)
+
+button.addEventListener("click", () => {
+    let userAns = prompt("How many number of squares?");
+
+    if (userAns == null) {
+        return
+    }
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.lastChild)
+    }
+
+    for (let i = 0; i < userAns; i++) {
+        for (let k = 0; k < userAns; k++) {
+            const newSquare = document.createElement("div");
+            newSquare.className = "square"
+            gridContainer.appendChild(newSquare);
+        }
+    }
+
+    startSketching()
+
+
 });
+
+
+function startSketching() {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(square => {
+        square.addEventListener("mouseenter", () => {
+            square.style.backgroundColor = "black"; // Color stays black after hover
+        });
+    }
+    )
+}
